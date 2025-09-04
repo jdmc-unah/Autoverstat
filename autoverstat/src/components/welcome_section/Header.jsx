0198c3bbf6493b5ec7e730c5d447e4914299b850
Daniel Mejia <daniel.cuellar@unah.hn>
2025-09-04 08:51:57 -0600

    menu para telefono con drawer

--- autoverstat/src/components/welcome_section/Header.jsx
+++ autoverstat/src/components/welcome_section/Header.jsx
@@ -3,19 +3,27 @@ import Typography from '@mui/material/Typography'
 import Box from '@mui/material/Box'
 import Button from '@mui/material/Button'
 import useMediaQuery from '@mui/material/useMediaQuery'
-import { useTheme } from '@emotion/react'
+import { useTheme} from '@emotion/react'
+import MenuRoundedIcon from '@mui/icons-material/MenuRounded';
+import IconButton from '@mui/material/IconButton'
+import { useState } from 'react'
 
 
-
-
+import MenuDrawer from './MenuDrawer'
 
 
 const Header = ()=>{
+    //evalua si es movil para habilitar otro tipo de menu
     const theme = useTheme();
     const isMobile = useMediaQuery(theme.breakpoints.down('md'))
 
+    // estado del drawer
+    const [open, setOpen] = useState(false);
 
+    const opciones = [{titulo : 'Más Información', accion: '#'}, {titulo : 'Servicios', accion: '#'}, 
+    {titulo : 'Contacto', accion: '#'} , {titulo :'Galería', accion: '#'}]
 
+    
     return(
         <Box sx={{display: 'flex', justifyContent:'space-between'  }} >
            
@@ -24,17 +32,19 @@ const Header = ()=>{
                 <Typography variant="body1" color="var(--negro)">Autoverstat</Typography>
             </Box>
 
-           {isMobile ?
+           {isMobile 
+                ?
                 <Box>
-                    <Typography variant="body1" color="initial"></Typography>
+                    <IconButton aria-label="" onClick={()=>{setOpen(!open)}}>
+                      <MenuRoundedIcon/>
+                    </IconButton>
+                    <MenuDrawer open={open} setOpen={setOpen} opciones={opciones}/>
                 </Box>   
-                    :
+                :
                 <Box sx={{display: 'flex', justifyContent: 'space-between', minWidth: 500  }}>
-                    <Button color="var(--negro)" sx={{textTransform:'none'}}>Más Información</Button>
-                    <Button color="var(--negro)" sx={{textTransform:'none'}}>Servicios</Button>
-                    <Button color="var(--negro)" sx={{ textTransform:'none'}}>Contacto</Button>
-                    <Button color="var(--negro)" sx={{ textTransform:'none'}}>Galería</Button>
-                
+                    {opciones.map((opcion, indice)=>(
+                        <Button key={indice}  color="var(--negro)" sx={{textTransform:'none'}}>{opcion.titulo}</Button>
+                    ))}
                 </Box>      
             }
             
---
+++ autoverstat/src/components/welcome_section/MenuDrawer.jsx
@@ -0,0 +1,39 @@
+import Box from '@mui/material/Box';
+import Drawer from '@mui/material/Drawer';
+import Button from '@mui/material/Button';
+import List from '@mui/material/List';
+import ListItem from '@mui/material/ListItem';
+import ListItemButton from '@mui/material/ListItemButton';
+import ListItemText from '@mui/material/ListItemText';
+
+const MenuDrawer = ({open, setOpen, opciones})=> {
+  
+  const handleClose =()=>{
+    setOpen(false);
+  }
+
+
+  const DrawerList = (
+    <Box sx={{ width: 170 }} role="presentation" onClick={handleClose}>
+      <List>
+        {opciones.map((opcion, indice)=>(
+          <ListItem key={indice} disablePadding>
+            <ListItemButton>
+                <ListItemText primary={opcion.titulo} />
+            </ListItemButton>
+          </ListItem>
+        ))}
+      </List>
+    </Box>
+  );
+
+  return (
+    <div>
+      <Drawer anchor='right' open={open} onClose={handleClose}>
+        {DrawerList}
+      </Drawer>
+    </div>
+  );
+}
+
+export default MenuDrawer
