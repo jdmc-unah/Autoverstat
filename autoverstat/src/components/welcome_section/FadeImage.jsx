1d2a0353f20ee22f824889b57a240a007c687a93
Daniel Mejia <daniel.cuellar@unah.hn>
2025-09-04 10:29:14 -0600

    imagen de bienvenida con fade

---
+++ autoverstat/src/components/welcome_section/FadeImage.jsx
@@ -0,0 +1,55 @@
+import { useEffect, useState } from 'react';
+import { Fade, Box } from '@mui/material';
+
+import blueCarCrashed from '../../assets/blueCarCrashed.png'
+import blueCarFinished from '../../assets/blueCarFinished.png'
+
+const FadeImage = () => {
+  const [mostrarPrimera, setMostrarPrimera] = useState(true);
+
+  useEffect(() => {
+    const intervalo = setInterval(() => {
+      setMostrarPrimera((prev) => !prev);
+    }, 3000); // Cambia cada 3 segundos
+
+    return () => clearInterval(intervalo); // Limpieza al desmontar
+  }, []);
+
+  return (
+    <Box sx={{ position: 'relative', width: 450, height: 300,   
+    overflow: 'hidden' , backgroundColor: 'var(--naranja)', borderRadius: 1.5}}>
+      
+      <Fade in={mostrarPrimera} timeout={1000}>
+        <Box
+          component="img"
+          src={blueCarCrashed}
+          
+          alt="Imagen 1"
+          sx={{
+            pl:1,
+            position: 'absolute',
+            width: '100%',
+            height: '100%',
+            objectFit: 'cover',
+          }}
+        />
+      </Fade>
+
+      <Fade in={!mostrarPrimera} timeout={1000}>
+        <Box
+          component="img"
+          src={blueCarFinished}
+          alt="Imagen 2"
+          sx={{
+            position: 'absolute',
+            width: '100%',
+            height: '100%',
+            objectFit: 'cover',
+          }}
+        />
+      </Fade>
+    </Box>
+  );
+};
+
+export default FadeImage;
--- autoverstat/src/components/welcome_section/Welcome.jsx
+++ autoverstat/src/components/welcome_section/Welcome.jsx
@@ -3,20 +3,30 @@ import Typography from '@mui/material/Typography'
 import Box from '@mui/material/Box'
 import Button from '@mui/material/Button'
 
-
-
+import Details from './Details'
+import FadeImage from './FadeImage'
 
 const Welcome =()=>{
 
     return(
-        <Box sx={{  mt: 10, display:'flex', alignItems:'center', flexDirection:'column'}} >
-            <Typography textAlign={'center'} variant="h4" color="var(--negro)">Donde la experiencia se convierte en excelencia</Typography>
-            <Typography sx={{pt:2, pb:4}} variant="body1" color="text.secondary">Acabados de la mejor calidad para tu vehículo</Typography>
-            <Button   size='medium' variant="contained" sx={{backgroundColor: "var(--rojo)", textTransform: 'none'}}>
-              Empezar
-            </Button>
+        
+        <>
+            <Box sx={{  mt: 10, display:'flex', alignItems:'center', flexDirection:'column'}} >
+                <Typography textAlign={'center'} variant="h4" color="var(--negro)">Donde la experiencia se convierte en excelencia</Typography>
+                <Typography sx={{pt:2, pb:4}} variant="body1" color="text.secondary">Acabados de la mejor calidad para tu vehículo</Typography>
+                <Button   size='medium' variant="contained" sx={{backgroundColor: "var(--rojo)", textTransform: 'none'}}>
+                    Empezar
+                </Button>
+            </Box>
 
-        </Box>
+            <Box sx={{display:'flex', justifyContent:'space-evenly', alignItems: 'top', mt:5}}>
+                <Details texto={ `20+ \n Años de Servicio`}/>
+                <FadeImage/>
+                <Details texto={ `20+ \n Años de Servicio`}/>
+            </Box>
+
+        </>
+        
     )
 }
 
